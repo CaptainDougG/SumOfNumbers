11efb3920a351a2bc7b142b3f423a00e483028ac
904238003 <904238003@L829WD150099.edu.cbe.ab.ca>
2021-03-16 14:42:56 -0600

    Change Iterator Name

--- SumOfNumbers/src/SumOfNumbers.java
+++ SumOfNumbers/src/SumOfNumbers.java
@@ -4,15 +4,15 @@ public class SumOfNumbers {
 	public static String getSum(int start, int end) {
 		String equation = "";
 		int sum = 0;
-		int numberOfLoops;
+		int i;
 		if (start <= end) {
-			for (numberOfLoops = start; numberOfLoops <= end; numberOfLoops++) {
-				sum = sum + numberOfLoops;
-				if (numberOfLoops != start) {
-				equation = equation + " + " + numberOfLoops;
+			for (i = start; i <= end; i++) {
+				sum = sum + i;
+				if (i != start) {
+					equation = equation + " + " + i;
 				}
 				else {
-					equation = equation + numberOfLoops;
+					equation = equation + i;
 				}
 			}
 			return equation + " = " + sum;
